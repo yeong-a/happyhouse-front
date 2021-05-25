3139b3c8441601b248cf2a3454e2ddf1e3b294e2
Yeong-a Jang <jya005@gmail.com>
2021-05-25 19:37:53 +0900

    add consts.js

---
+++ src/components/QnAList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <tr>
+      <td>{{ no }}</td>
+      <td>
+        <router-link :to="`/read/${no}`">{{ title }}</router-link>
+      </td>
+      <td>{{ writer }}</td>
+      <td>{{ getFormatDate(regtime) }}</td>
+    </tr>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
